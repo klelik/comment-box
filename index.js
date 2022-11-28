85c9ac968ea824f93e50c6719cb81116654c276a
klelik <klelika1998@gmail.com>
2022-11-28 12:48:52 +0000

    add block function when word length exceeded

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-var wordCount = document.getElementById('count');
-var valid = document.getElementById('valid');
+var wordCount = document.getElementById("count");
+var valid = document.getElementById("valid");
 var butt = document.getElementById("sub-btn");
 
+let countLength = 0;
 
-comment.addEventListener('keyup', function(){
-var characters = comment.value.split('');
-let commLength = characters.length;
+comment.addEventListener("keyup", function () {
+  var characters = comment.value.split("");
+  let commLength = characters.length;
 
-wordCount.innerText = commLength;
+  globalLength = commLength;
 
-if (commLength <10 && commLength > 3) {
+  wordCount.innerText = commLength;
+
+  if (commLength < 139 && commLength > 3) {
     wordCount.classList = "valid";
     valid.classList = "valid";
     valid.innerText = "valid input";
-
-
-
-} else{
-    valid.innerText = "Type more than 3 and less than 140 characters please!";
+  } else {
+    valid.innerText = "Type less than 140 characters please!";
     wordCount.classList = "invalid";
     valid.classList = "invalid";
-
-
-}
-
-
+  }
 });
 
-function add(){
-var newCom = document.createElement("div")
-var comment = document.getElementById("comment").value;
-var name = document.getElementById("name").value;
-var email = document.getElementById('email').value;
+butt.addEventListener("click", function () {
+  if (globalLength > 13) {
+  } else if (globalLength < 4) {
+    alert("Type more than 3 characters please!");
+  }
+});
 
+function add() {
+  var newCom = document.createElement("div");
+  var comment = document.getElementById("comment").value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
 
-var txtnode = document.createTextNode("Author : "+name+  " email : "+email+  "comment : " +comment );
+  var txtnode = document.createTextNode(
+    "Author : " + name + " email : " + email + "comment : " + comment
+  );
 
+  newCom.appendChild(txtnode);
 
-newCom.appendChild(txtnode);
-
-document.getElementById("lists").appendChild(newCom);
+  document.getElementById("lists").appendChild(newCom);
 }
-
-
